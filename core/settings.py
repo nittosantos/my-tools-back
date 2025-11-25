a022e54b75345b71618927d5430c9e7173c4df0a
nittosantos <nittosantos.dev@gmail.com>
2025-11-25 18:22:42 -0300

    Corrige collectstatic: cria diretório e usa storage mais simples

--- core/settings.py
+++ core/settings.py
@@ -134,7 +134,8 @@ STATICFILES_DIRS = [
 ]
 
 # WhiteNoise para servir arquivos estáticos em produção
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+# Usa CompressedStaticFilesStorage (mais simples) em vez de CompressedManifestStaticFilesStorage
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
